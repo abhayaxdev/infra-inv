06c22da1fbe9dbb8c8b0aaed5210c902a2fe02d2
abhaya <gautamabhaya98@gmail.com>
2026-07-29 00:32:18 +0545

    wip(dashboard): working on the layout

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from core.views import Dashboard
+
 app_name = "core"
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="core/landing.html"), name="landing"),
-    path("dashboard/", TemplateView.as_view(template_name="core/dashboard.html"), name="dashboard"),
+    path("dashboard/", Dashboard.as_view(), name="dashboard"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.views.generic import TemplateView
+from django.shortcuts import render
+
+from organizations.models import Project
+
+
+class Dashboard(TemplateView):
+    template_name = "core/dashboard.html"
+
+    def get(self, request, *args, **kwargs):
+        context  = self.get_context_data()
+        context["projects"] = Project.objects.all()        
+        return render(self.request, self.template_name, context)
