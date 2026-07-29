fbb92211440e2a949648141c1d8d0295e33b24d4
abhaya <gautamabhaya98@gmail.com>
2026-07-29 23:33:24 +0545

    fix: debug toolbar config was missing ips

--- project/settings.py
+++ project/settings.py
@@ -33,6 +33,8 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
+INTERNAL_IPS = env.list('INTERNAL_IPS', default=['127.0.0.1'])
+
 ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
